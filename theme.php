eb9c0364aa3dddd5b3f6944458828e1b6c66d2c0
tommy tomson <thomas.bierey@gmx.de>
2012-04-25 20:41:18 +0200

    updated to jqeuery mobile 1.10

--- theme.php
+++ theme.php
@@ -20,10 +20,8 @@ function mobile_init(&$a) {
 	$cssFile = $a->get_baseurl($ssl_state)."/view/theme/mobile/jquerymobile.css";
    $a->page['htmlhead'] .= sprintf('<link rel="stylesheet" type="text/css" href="%s" />', $cssFile);
 	
-	$a->page['htmlhead'] .= <<<EOT
-	<script type="text/javascript" src="http://code.jquery.com/mobile/1.0b1/jquery.mobile-1.0b1.min.js"></script>
-	
-EOT;
+	$mobileJS = $a->get_baseurl($ssl_state)."/view/theme/mobile/js/jquery.mobile-1.1.0.js";
+   $a->page['htmlhead'] .= sprintf('<script type="text/javascript" src="%s"></script>', $mobileJS);
 
 }
 
